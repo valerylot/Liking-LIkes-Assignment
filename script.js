dae71df49c1e300888aed20eef890961fadd9469
valerylot <lotvalery@gmail.com>
2023-12-12 10:27:26 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// var btn1=document.querySelector(".btn1");
+// var counter1=document.querySelector(".counter1");
+// var a=9;
+// btn1.addEventListener("click", ()=>{
+//     a++;
+//     counter1.innerText=a;
+// })
+
+// var btn2=document.querySelector(".btn2");
+// var counter2=document.querySelector(".counter2");
+// var b=12;
+// btn2.addEventListener("click", ()=>{
+//     b++;
+//     counter2.innerText=b;
+// })
+
+// var btn3=document.querySelector(".btn3");
+// var counter3=document.querySelector(".counter3");
+// var c=9;
+// btn3.addEventListener("click", ()=>{
+//     c++;
+//     counter3.innerText=c;
+// })
+
+// LIKE BUTTON 1
+var counter1 = document.querySelector(".counter1");
+var btn1 = counter1.innerText;
+
+function btnClicks() {
+    btn1++;
+    counter1.innerText = btn1;
+}
+
+// LIKE BUTTON 2
+var counter2 = document.querySelector(".counter2");
+var btn2 = counter2.innerText;
+
+function btnClicks2() {
+    btn2++;
+    counter2.innerText = btn2;
+}
+
+// LIKE BUTTON 3
+var counter3 = document.querySelector(".counter3");
+var btn3 = counter3.innerText;
+
+function btnClicks3() {
+    btn3++;
+    counter3.innerText = btn3;
+}
